458d35324802688f538380f4df4c809ab670600c
erdogan cayir <erdogancayir@erdogans-MacBook-Pro.local>
2025-02-06 08:44:32 +0300

    ok

--- src/main/java/com/avaj/Main.java
+++ src/main/java/com/avaj/Main.java
@@ -5,6 +5,41 @@ import com.avaj.database.HeroManager;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static final String GUI_MODE = "gui";
+    private static final String CONSOLE_MODE = "console";
+
     public static void main(String[] args) {
+        HeroManager heroManager = new HeroManager();
+        heroManager.createHeroesTable();
+
+        boolean guiMode = parseArguments(args);
+
+        if (guiMode) {
+            System.out.println("Launching GUI mode...");
+        } else {
+            System.out.println("Launching Console mode...");
+        }
+    }
+
+    private static boolean parseArguments(String[] args) {
+        if (args.length != 1) {
+            printUsage();
+        }
+
+        return switch (args[0].toLowerCase()) {
+            case GUI_MODE -> true;
+            case CONSOLE_MODE -> false;
+            default -> {
+                printUsage();
+                throw new IllegalArgumentException("Invalid game mode: " + args[0]);
+            }
+        };
+    }
+
+    private static void printUsage() {
+        System.out.println("Choose the game mode by providing an argument.");
+        System.out.println("\t$ java -jar swingy-1.0-jar-with-dependencies.jar console");
+        System.out.println("\t$ java -jar swingy-1.0-jar-with-dependencies.jar gui");
+        System.exit(1);
     }
 }
--- src/main/java/com/avaj/database/DbInterface.java
+++ src/main/java/com/avaj/database/DbInterface.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public abstract class DbInterface {
     private static final String JDBC_DRIVER = "org.h2.Driver";
-    private static final String DB_URL = "jdbc:h2:/home/ecayir/IdeaProjects/Swingy/src/main/resources/HeroesDB/HeroesH2";
+    private static final String DB_URL = "jdbc:h2:./src/main/resources/HeroesDB/HeroesH2";
     private static final String USER = "sa";
     private static final String PASS = "";
     //private static final String DB_URL = "jdbc:h2:mem:testdb";
